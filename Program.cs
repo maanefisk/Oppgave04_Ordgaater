d57c48e5d9761e9b3562ef9d12af57a5fdde2abd
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-25 17:07:26 +0200

    jobber nedover

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace Oppgave04_Ordgaater
 {
@@ -7,14 +9,48 @@ namespace Oppgave04_Ordgaater
     {
         static void Main(string[] args)
         {
-            var path = @"E:\Work\_Modul3\Oppgave04_Ordgaater\Oppgave04_Ordgaater\ordliste.txt";
-            var lines = File.ReadLines(path);
-            foreach (var line in lines)
+            var words = GetWords(); // Får ut liste med ord
+            var randomWordIndex = new Random().Next(words.Length); //Velger ett random ord, og nummeret dens i lista
+            var selectedWord = words[randomWordIndex]; 
+            Console.WriteLine("Valgt ord: " + selectedWord);
+
+            for (var i = 0; i < words.Length; i++)
             {
-                var parts = line.Split('\t');
-                var word = parts[1];
-                Console.WriteLine(word);
+                if (i % 1000 == 0) Console.Write(".");
+
+                if (LastPartIsSameAsFirstPartSecondWord(selectedWord, words[i]))
+                {
+                    Console.WriteLine("\n" + words[i]);
+                    return;
+                }
             }
+        }
+
+        private static bool LastPartIsSameAsFirstPartSecondWord(string word1, string word2)
+        {
+            return LastPartIsSameAsFirstPartSecondWord(2, word1, word2)
+        }
+        private static bool LastPartIsSameAsFirstPartSecondWord(int commonLength, string word2)
+        {
+            var lastPartOfFirstWord = word1.Substring(word1.length - commonLength, commonLength);
+            var firstPartOfSecondWord = word2.Substring(0, commonLength);
+
+            return lastPartOfFirstWord == firstPartOfSecondWord;
+        }
+        static string[] GetWords()
+        {
+            var lastWord = string.Empty;
+            var path = @"E:\Work\_Modul3\Oppgave04_Ordgaater\Oppgave04_Ordgaater\ordliste.txt";
+            var wordList = new List<string>();
+            foreach (var line in File.ReadLines(path, Encoding.UTF8))
+            {
+                var parts = line.Split('\t'); //fjerner tab mellomrom og separerer string, hvert ord til plass i en array. 1 array pr linje.
+                var word = parts[1]; //henter ut ord på plassering 1 i "linje-arrayet"
+                if (word != lastWord && word.Length > 7 && !word.Contains("-")) wordList.Add(word); //hvis ordet ikke er det samme som forrige valgt, er større en 7 tegn, uten bindestrek, da addes den til lista.
+                lastWord = word; 
+            }
+
+            return wordList.ToArray();
         }
     }
 }
